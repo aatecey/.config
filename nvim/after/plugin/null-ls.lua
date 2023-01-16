ac3f053e4f2acab2ca173f8a28a345c2ab2cc708
atecey <atecey@gmail.com>
2023-01-16 14:24:43 +0000

    feat: adding the lap formatter to run on save

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -2,11 +2,25 @@ local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
   sources = {
     formatting.prettierd,
     formatting.stylua,
     diagnostics.eslint_d
-  }
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
+
